66dff948bcc46fe5c3412b78c91e2e9adcd3ee6b
Douglas <douglas.olvr8@gmail.com>
2024-08-12 16:00:03 -0300

    Att C composta

---
+++ src/ResultadoEscolarComposta.java
@@ -0,0 +1,11 @@
+public class ResultadoEscolarComposta {
+    public static void main(String[] args) {
+        
+        int nota = 6;
+
+        if (nota >= 8){
+            System.out.println("Aprovador");
+        }else 
+            System.out.println("Reprovador");
+    }
+}
